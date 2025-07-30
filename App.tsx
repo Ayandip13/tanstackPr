822013178c2f7bdd023e7787f7528d41b98cbf2d
ayandip <ayandip.paul@weavers-web.com>
2025-07-30 17:01:30 +0530

    add mutation functions

--- App.tsx
+++ App.tsx
@@ -1,10 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, ListRenderItem, TextInput } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { createTodo, getTodos, Todo } from './api/todo'
+import { useMutation } from '@tanstack/react-query'
 
 const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const fetchTodos = async () => {
+    setLoading(true)
+    try {
+      const response = await getTodos()
+      setTodos(response)
+      setError(null)
+    } catch (error: any) {
+      console.log(error);
+      setError(error.message || "Unknown error")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const addMutation = useMutation({
+    mutationFn: createTodo,
+    onSuccess: (data) => {
+      console.log("Success", data);
+    }
+  })
+
+  const handleTodo = () => {
+    addMutation.mutate(todos)
+    setTodos("")
+  }
+
+  const renderTodo: ListRenderItem<Todo> = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.todos}</Text>
+      </View>
+    )
+  }
+
+  if (loading) return <Text>Loading...</Text>
+  if (error) return <Text>{error}</Text>
+
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TextInput
+        onChangeText={setTodos}
+        value={todos}
+        placeholder='Add a Todo'
+      />
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderTodo}
+      />
     </View>
   )
 }
--- api/todo.ts
+++ api/todo.ts
@@ -1,4 +1,5 @@
-const API_URL = process.env.RN_PUBLIC_API_URL;
+// const API_URL = process.env.RN_PUBLIC_API_URL;
+const API_URL = 'http://localhost:3000';
 
 export interface Todo {
     id: string,
@@ -9,5 +10,50 @@ export interface Todo {
 export async function getTodos(): Promise<Todo[]> {
     const response = await fetch(`${API_URL}/todos`)
     const data = await response.json()
+    return data
+}
+
+export async function createTodo(text: string): Promise<Todo> {
+    const todo = {
+        id: Date.now().toString(),
+        todo: text,
+        completed: false
+    }
+
+    const response = await fetch(`${API_URL}/todos`, {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(todo),
+    })
+
+    const data = response.json()
+    return data
+}
+
+export async function deleteTodo(id: string): Promise<void> {
+    const result = await fetch(`${API_URL}/todos${id}`, {
+        method: "DELETE"
+    })
+    const data = result.json()
+    return data
+}
+
+export async function updateTodo(todo: Todo): Promise<Todo> {
+    const result = await fetch(`${API_URL}/todos/${todo.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    const data = result.json()
+    return data
+}
+
+export async function getTodosById(id: string): Promise<Todo> {
+    const result = await fetch(`${API_URL}/todos/${id}`)
+    const data = result.json()
     return data
 }
