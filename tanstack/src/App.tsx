c5b6c5ac7f8e6c16ebae1247fad8c860a541c5c9
ayandip <ayandip.paul@weavers-web.com>
2025-08-13 12:49:35 +0530

    add useQuery implementations to getting datas

--- tanstack/src/App.tsx
+++ tanstack/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: getTodos
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{data.map((todo: any) => <p>{todo.title}</p>)}</div>
     </>
   )
+}
+
+const getTodos = async () => {
+  const resp = await axios.get('https://jsonplaceholder.typicode.com/todos')
+  return resp.data
 }
 
 export default App
